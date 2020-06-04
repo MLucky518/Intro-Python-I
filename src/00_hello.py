5f59273e51ed6a9b19d84f879c0ac21d0ef6e794
MLucky518 <lucky32815@gmail.com>
2020-06-03 22:17:03 -0400

    mvp

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,5 @@
 # Print "Hello, world!" to your terminal
+
+#python 2 print "Hello, World!"  
+
+print("Hello,World")  #python3
--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,15 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("x is %d, y is %f, z is %s!" % (x, y, z))
+
 # Use the 'format' string method to print the same thing
 
+text = "x is {a}, y is {b}, z is {c}"
+
+print(text.format(a=10, b=2.24552, c="i like turtles"))
+
+
 # Finally, print the same thing using an f-string
+
+print(f"x is {x}, y is {y}, z is {z}")
